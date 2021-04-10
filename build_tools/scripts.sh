c9910ed13f9fc1fb88b15272dbcad31c6b109f4d
Alex Zaslavskis <sahsariga111@gmail.com>
2021-04-11 01:04:44 +0300

    Update scripts.sh

--- build_tools/scripts.sh
+++ build_tools/scripts.sh
@@ -8,8 +8,8 @@
 
 #/bin/sh
 
-wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/init_script/init.sh -O init.sh 
-wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/load_drone_code/load_code.sh -O init_drone.sh
+wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/init_script/init.sh -O loading.sh 
+wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/load_drone_code/load_code.sh -O loading_drone.sh
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/tools/version -O version
 chmod -R 777 init.sh
 chmod -R 777 init_drone.sh
