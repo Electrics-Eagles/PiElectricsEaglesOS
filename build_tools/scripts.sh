e37bf17c8c42eba0dc739491501f88f3e58d8903
Alex Zaslavskis <sahsariga111@gmail.com>
2021-04-15 23:34:29 +0300

    Update scripts.sh

--- build_tools/scripts.sh
+++ build_tools/scripts.sh
@@ -11,6 +11,8 @@
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/init_script/init.sh -O loading.sh 
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/load_drone_code/load_code.sh -O loading_drone.sh
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/tools/version -O version
+wget https://github.com/Electrics-Eagles/PiElectricsEagles/releases/download/v1-12.04.2021/pielectricseagles
 chmod -R 777 loading.sh
 chmod -R 777 loading_drone.sh
 chmod -R 777 version
+chmod -R 777 pielectricseagles
