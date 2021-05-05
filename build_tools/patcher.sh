146822705066e1b0617a2bd780548c042291500d
Alex Zaslavskis <sahsariga111@gmail.com>
2021-05-05 10:30:25 +0100

    Update patcher.sh

--- build_tools/patcher.sh
+++ build_tools/patcher.sh
@@ -1,14 +1,14 @@
 
 rm -rf S99app
-echo Patcher V1.0 Beta 
+#echo Patcher V1.0 Beta 
 echo This Script will create  your /etc/init.d scrpit 
-echo "#!/bin/sh " > S99app
+#echo "#!/bin/sh " > S99app
 
-echo "# Loading the modules needed by my app:" >> S99app
+#echo "# Loading the modules needed by my app:" >> S99app
 
  
-echo "# Launching my app: ">> S99app
-echo "/usr/bin/loading.sh &  " >> S99app   
+#echo "# Launching my app: ">> S99app
+#echo "/usr/bin/loading.sh &  " >> S99app   
 
 echo "/usr/bin/loading.sh & " >> rcS
 
