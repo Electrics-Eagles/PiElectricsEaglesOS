1c272fcd6c4b3d516075a7bd874d0d369466ceec
Alex Zaslavskis <sahsariga111@gmail.com>
2021-09-22 21:38:34 +0100

    awesome

--- 00_clone_buildroot_install_deps.sh
+++ 00_clone_buildroot_install_deps.sh
@@ -23,44 +23,61 @@ echo set to configuration to raspberrypi zero
 make raspberrypi0_defconfig 
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/buildroot_config/.config -O .config
 chmod  755 .config
-
-
-
-
-cat > rcS << EOF
+cat > ./buildroot-2021.05/output/target/etc/init.d/rcS << EOF
 #/bin/sh
 # Electrics-Eagles team 2021 for PiElectricsEagles 
 #
 #
 #
 # Under MIT Licence 
-
 /usr/bin/pielectricseagles.sh &
 EOF
-cd .. 
+rm  -rf ./buildroot-2021.05/output/target/etc/inittab
+cat >  ./buildroot-2021.05/output/target/etc/inittab << EOF
+# /etc/inittab
+#
+# Copyright (C) 2001 Erik Andersen <andersen@codepoet.org>
+#
+# Note: BusyBox init doesn't support runlevels.  The runlevels field is
+# completely ignored by BusyBox init. If you want runlevels, use
+# sysvinit.
+#
+# Format for each entry: <id>:<runlevels>:<action>:<process>
+#
+# id        == tty to run on, or empty for /dev/console
+# runlevels == ignored
+# action    == one of sysinit, respawn, askfirst, wait, and once
+# process   == program to run
 
+# Startup the system
+::sysinit:/bin/mount -t proc proc /proc
+::sysinit:/bin/mount -o remount,rw /
+::sysinit:/bin/mkdir -p /dev/pts /dev/shm
+::sysinit:/bin/mount -a
+::sysinit:/sbin/swapon -a
+null::sysinit:/bin/ln -sf /proc/self/fd /dev/fd
+null::sysinit:/bin/ln -sf /proc/self/fd/0 /dev/stdin
+null::sysinit:/bin/ln -sf /proc/self/fd/1 /dev/stdout
+null::sysinit:/bin/ln -sf /proc/self/fd/2 /dev/stderr
+::sysinit:/bin/hostname -F /etc/hostname
+# now run any rc scripts
+::sysinit:/etc/init.d/rcS
 
+# Put a getty on the serial port
+::respawn:-/bin/sh
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console
 
-sudo cp  ./drivers/ch340_package_buildroot.zip ./rtc/ch340_package_buildroot.zip
+# Stuff to do for the 3-finger salute
+#::ctrlaltdel:/sbin/reboot
 
+# Stuff to do before rebooting
+::shutdown:/etc/init.d/rcK
+::shutdown:/sbin/swapoff -a
+::shutdown:/bin/umount -a -r
 
-
+EOF
+sudo cp ./scripts/pielectricseagles.sh  ./buildroot-2021.05/output/target/usr/bin/pielectricseagles.sh
 unzip ch340_package_buildroot.zip
-unzip rtl8153_package_buildroot.zip
-
-
+sudo cp -R ch340 ./buildroot-2021.05/package
 rm -rf ch340_package_buildroot.zip
-rm -rf rtl8153_package_buildroot.zip
-
-
-
-
-
-wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/init_script/init.sh -O loading.sh 
-wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/perpare_takeoff/init.sh -O loading_drone.sh
-wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/tools/version -O version
-wget https://github.com/Electrics-Eagles/PiElectricsEagles/releases/download/v1-12.04.2021/pielectricseagles
-chmod -R 777 loading.sh
-chmod -R 777 loading_drone.sh
-chmod -R 777 version
-chmod -R 777 pielectricseagles
+make > build_log.txt
