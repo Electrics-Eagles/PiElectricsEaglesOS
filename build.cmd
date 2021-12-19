699a781465781795c7ee51d7ce5a11b216f53740
Alex Zaslavskis <sahsariga111@gmail.com>
2021-12-19 15:59:58 +0000

    s

---
+++ build.cmd
@@ -0,0 +1,1 @@
+docker build C:\Users\payan\Documents\GitHub\PiElectricsEaglesOS
---
+++ pielectricseagles.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+# Electrics-Eagles team 2021 for PiElectricsEagles 
+#
+#
+#
+# Under MIT Licence 
+/usr/bin/pielectricseagles.sh &
