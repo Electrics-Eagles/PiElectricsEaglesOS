8cb993e22bce1097244d458a1d96a1dc2ff555af
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-06 00:51:21 +0200

    Update post-build.sh

--- post_build/post-build.sh
+++ post_build/post-build.sh
@@ -28,6 +28,7 @@ mv config.ini ${TARGET_DIR}/etc/pielectricseagles/config.ini
 
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_build/version.sh
 mv version.sh ${TARGET_DIR}/usr/bin/version.sh
+chmod 777   ${TARGET_DIR}/usr/bin/version.sh
 rm -rf version.sh
 
 echo Done.
