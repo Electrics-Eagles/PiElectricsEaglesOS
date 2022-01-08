ab7d6c197296dceaa94b24a2347cca93e82cdcf4
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-08 08:40:40 -0800

    Update post-image.sh

--- post_image/post-image.sh
+++ post_image/post-image.sh
@@ -1,9 +1,11 @@
 set -e 
 
+
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
 
 
 CONFIG_TXT=./output/images/rpi-firmware/config.txt
@@ -61,13 +63,14 @@ cp bootcode.bin $boot_bin
  
 trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
-rm -rf "${GENIMAGE_TMP}"
-genimage \
 
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
 	--rootpath "${ROOTPATH_TMP}"   \
 	--tmppath "${GENIMAGE_TMP}"    \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
 	--config "${GENIMAGE_CFG}"
-exit $?
 
+exit $?
