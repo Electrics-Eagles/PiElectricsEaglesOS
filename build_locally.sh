d15fa7381f9153d1286edbeaf7db29b29761096a
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-08 05:05:04 -0800

    Update build_locally.sh

--- build_locally.sh
+++ build_locally.sh
@@ -49,22 +49,22 @@ sudo export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
 #Sometimes Buildroot need proper locale, e.g. when using a toolchain
 # based on glibc.
 sudo  locale-gen en_US.utf8
-sudo curl  https://github.com/Electrics-Eagles/PiElectricsEaglesOS/raw/master/buildroot-2021.11.tar.gz >  buildroot.tar.gz
-sudo tar -xzvf  buildroot.tar.gz
+sudo wget  https://github.com/Electrics-Eagles/PiElectricsEaglesOS/raw/master/buildroot-2021.11.tar.gz 
 
+sudo tar -xzvf  buildroot-2021.11.tar.gz 
 sudo chmod 777  buildroot-2021.11
 cd buildroot-2021.11
-sudo curl  https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_build/post-build.sh > post-build.sh
+sudo wget  https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_build/post-build.sh
 sudo mv post-build.sh ./board/raspberrypi/post-build.sh
 sudo chmod 777 ./board/raspberrypi/post-build.sh
-sudo curl https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_image/post-image.sh > post-image.sh
+sudo wget  https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_image/post-image.sh
 sudo mv post-image.sh ./board/raspberrypi/post-image.sh
 sudo chmod 777 ./board/raspberrypi/post-image.sh
-sudo curl https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_image/genimage-raspberrypi0.cfg >  genimage-raspberrypi0.cfg
+sudo wget  https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_image/genimage-raspberrypi0.cfg
 sudo mv genimage-raspberrypi0.cfg  ./board/raspberrypi/genimage-raspberrypi0.cfg
 
 sudo make raspberrypi0_defconfig
 sudo chmod 777 .config
 rm -rf .config
-sudo curl https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/.config >  .config
+sudo wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/.config
 sudo make all -j8 >> log.txt
