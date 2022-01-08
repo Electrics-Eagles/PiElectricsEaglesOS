96e8f3691385a0b2626ae8f04a779c6cd8d04d39
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-08 04:29:17 -0800

    Update post-build.sh

--- post_build/post-build.sh
+++ post_build/post-build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-
 set -u
 set -e
 echo PiElectricsEaglesOS build.
-
-
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_build/clone.py
 /usr/bin/python3 clone.py
 mv pielectricseagles ${TARGET_DIR}/usr/bin/pielectricseagles
@@ -15,25 +12,17 @@ wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/mast
 mv inittab ${TARGET_DIR}/etc/inittab
 echo initab copied
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/pielectricseagles.sh
-
 mv pielectricseagles.sh ${TARGET_DIR}/usr/bin/init.sh
 chmod 777  ${TARGET_DIR}/usr/bin/init.sh
 echo patch rcS file
 echo  "/usr/bin/init.sh & ">> ${TARGET_DIR}/etc/init.d/rcS
 chmod 777  ${TARGET_DIR}/etc/init.d/rcS
-
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEagles-dev/master/src/config/config.ini
-mkdir ${TARGET_DIR}/etc/pielectricseagles
 mv config.ini ${TARGET_DIR}/etc/pielectricseagles/config.ini
-
 wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEaglesOS/master/post_build/version.sh
 mv version.sh ${TARGET_DIR}/usr/bin/version.sh
 chmod 777   ${TARGET_DIR}/usr/bin/version.sh
 rm -rf version.sh
 echo Config install 
-wget https://raw.githubusercontent.com/Electrics-Eagles/PiElectricsEagles-dev/master/src/config/config.ini
-#mkdir   ${TARGET_DIR}/etc/pielectricseagles
-mv config.ini ${TARGET_DIR}/etc/pielectricseagles/config.ini
 chmod 777   ${TARGET_DIR}/etc/pielectricseagles/config.ini
-
 echo Done.
